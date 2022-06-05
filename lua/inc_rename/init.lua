adc01118c731c2ae703577550a372b0f6847a593
smjonas <jonas.strittmatter@gmx.de>
2022-06-05 14:32:43 +0200

    Refactor: use separate callback and preview functions

--- lua/inc_rename/init.lua
+++ lua/inc_rename/init.lua
@@ -11,7 +11,8 @@ local state = {
   lsp_positions = nil,
 }
 
-local function incremental_rename(opts, preview_ns, preview_buf)
+-- Called when the user is still typing the command or the command arguments
+local function incremental_rename_preview(opts, preview_ns, preview_buf)
   -- Store the lines of the buffer at the first invocation
   if state.should_fetch_references then
     -- Get positions of LSP reference symbols
@@ -35,7 +36,7 @@ local function incremental_rename(opts, preview_ns, preview_buf)
       state.orig_lines = {}
       for _, position in ipairs(state.lsp_positions) do
         local line_nr = position.start.line
-        local line = vim.api.nvim_buf_get_lines(buf, line_nr, line_nr + 1, 0)[1]
+        local line = vim.api.nvim_buf_get_lines(buf, line_nr, line_nr + 1, false)[1]
         local start_col, end_col = position.start.character, position["end"].character
         local line_item = { text = line, start_col = start_col, end_col = end_col }
         -- Same line was already seen, this case needs to be handled separately
@@ -66,64 +67,44 @@ local function incremental_rename(opts, preview_ns, preview_buf)
       -- Offset by the length difference between the new and old names
       offset = offset + #new_name - (item.end_col - item.start_col)
     end
-    if preview_ns then
-      vim.api.nvim_buf_set_lines(0, line_nr, line_nr + 1, false, { updated_line })
+    vim.api.nvim_buf_set_lines(0, line_nr, line_nr + 1, false, { updated_line })
+    if preview_buf then
+      vim.api.nvim_buf_set_lines(preview_buf, line_nr, line_nr + 1, false, { updated_line })
+    end
+    for _, hl_pos in ipairs(highlight_positions) do
+      vim.api.nvim_buf_add_highlight(0, preview_ns, M.config.hl_group, line_nr, hl_pos.start_col, hl_pos.end_col)
       if preview_buf then
-        vim.api.nvim_buf_set_lines(preview_buf, line_nr, line_nr + 1, false, { updated_line })
-      end
-      for _, hl_pos in ipairs(highlight_positions) do
         vim.api.nvim_buf_add_highlight(
-          0,
+          preview_buf,
           preview_ns,
           M.config.hl_group,
           line_nr,
           hl_pos.start_col,
           hl_pos.end_col
         )
-        if preview_buf then
-          vim.api.nvim_buf_add_highlight(
-            preview_buf,
-            preview_ns,
-            M.config.hl_group,
-            line_nr,
-            hl_pos.start_col,
-            hl_pos.end_col
-          )
-        end
       end
     end
   end
-  if not preview_ns then
-    -- Now execute the actual LSP rename command
-    vim.lsp.buf.rename(new_name)
-    state.should_fetch_references = true
-  end
+  return 2
+end
 
-  if preview_ns then
-    return 2
-  end
+-- Runs when the command is executed (user pressed enter)
+local function incremental_rename_execute(opts)
+  vim.lsp.buf.rename(opts.args)
+  state.should_fetch_references = true
 end
 
 local create_user_command = function(cmd_name)
   -- Create the user command
   vim.api.nvim_create_user_command(
     cmd_name,
-    incremental_rename,
-    { nargs = 1, addr = "lines", preview = incremental_rename }
+    incremental_rename_execute,
+    { nargs = 1, addr = "lines", preview = incremental_rename_preview }
   )
 end
 
 M.setup = function(user_config)
   M.config = vim.tbl_deep_extend("force", M.default_config, user_config or {})
-  local id = vim.api.nvim_create_augroup("inc_rename.nvim", { clear = true })
-  -- We need to be able to tell when the command was aborted to refetch the references.
-  -- Otherwise the same variable would be renamed every time.
-  vim.api.nvim_create_autocmd({ "CmdLineLeave" }, {
-    group = id,
-    callback = vim.schedule_wrap(function()
-      state.should_fetch_references = true
-    end),
-  })
   create_user_command(M.config.cmd_name)
 end
 
